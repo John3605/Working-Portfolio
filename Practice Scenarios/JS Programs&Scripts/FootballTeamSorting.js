78422d2e1902d798e92dc37e4ebcc0da5c7f234f
John3605 <john3605.live@gmail.com>
2023-02-24 15:57:50 -0800

    Create FootballTeamSorting.js

---
+++ Practice Scenarios/JS Programs&Scripts/FootballTeamSorting.js
@@ -0,0 +1,14 @@
+const player = {
+  playerName: "Leo",
+  goalsScored: 4,
+  gamesPlayed: 2
+};
+
+const team = {
+  teamName: "Lions",
+  numberPlayers: 18,
+  varsity: true,
+  member: player
+};
+
+console.log(team);
