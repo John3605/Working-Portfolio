fcf53cebc8f68d6d02bf9882aed1320f814fa66d
John3605 <john3605.live@gmail.com>
2023-02-25 16:53:12 -0800

    Create index.js

---
+++ PracticeSites/*WIP*DynamicCommentSection/index.js
@@ -0,0 +1,22 @@
+function createEnclosingDiv() {
+	
+	
+}
+
+function createAvatar(initials) {
+	
+}
+
+function createText(text) {
+	
+}
+
+function addComment() {
+ let commentsDiv = document.getElementById("commentSection");
+	let initialsInput = document.getElementById("initials").value;
+	let textInput = document.getElementById("commentText").value;
+
+	let enclosingDiv = createEnclosingDiv();
+	let avatar = createAvatar(initialsInput);
+	let text = createText(textInput);
+}
