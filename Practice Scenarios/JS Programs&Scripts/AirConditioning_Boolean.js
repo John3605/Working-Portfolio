27a73eb5744292a6716a978387d2de2886fce826
John3605 <john3605.live@gmail.com>
2023-03-01 23:43:51 -0800

    Update AirConditioning_Boolean.js

--- Practice Scenarios/JS Programs&Scripts/AirConditioning_Boolean.js
+++ Practice Scenarios/JS Programs&Scripts/AirConditioning_Boolean.js
@@ -2,11 +2,12 @@
  const airConditioning = !true;
  const highTemperature = true;
 
- console.log("Weekday");
+ console.log("Is this a weekday?");
  console.log(isWeekday);
 
- console.log("Air Conditioning");
+
+ console.log("Air Conditioning on?");
  console.log(airConditioning);
 
- console.log("High Temperature");
+ console.log("High Temperature?");
  console.log(highTemperature);
