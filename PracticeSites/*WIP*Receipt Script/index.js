fb44b03e8ab888662122d1016ca4c55aa88a074b
John3605 <john3605.live@gmail.com>
2023-03-14 15:32:00 -0700

    Update index.js

--- PracticeSites/*WIP*Receipt Script/index.js
+++ PracticeSites/*WIP*Receipt Script/index.js
@@ -1,8 +1,9 @@
-let customers = ["John S.", "Emma M.", "Frank T."];
+let customers = ["John S.", "Emma M.", "Frank T.", "Jonathan Chavarria"];
 let receipts = [
   ["eggs", "white bread", "milk"],
   ["milk", "cheese", "butter", "pancake mix", "flour"],
-  ["cereal", "whole wheat bread", "milk", "cheese", "eggs", "vanilla"]
+  ["cereal", "whole wheat bread", "milk", "cheese", "eggs", "vanilla"],
+  ["bacon", "free range eggs", "honey", "greens mix"]
 ];
 let prices = {
   "eggs": 3.50,
@@ -14,7 +15,11 @@ let prices = {
   "flour": 5.75,
   "cereal": 4.50,
   "whole wheat bread": 6.50,
-  "vanilla": 8.75
+  "vanilla": 8.75,
+  "bacon": 6.99,
+  "free range eggs": 11.99,
+  "honey": 14.99,
+  "greens mix": 8.49
 };
 
 function setElementText(id, text) {
@@ -23,16 +28,18 @@ function setElementText(id, text) {
 
 function populateTable() {
   for (let i = 0; i < customers.length; i++) {
-    customer = customers[i];
-    receipt = receipts[i];
-    receipt_list = "";
-    sum = 0;
+    let customer = customers[i];
+    let receipt = receipts[i];
+    let receipt_list = "";
+    let sum = 0;
     for (let j = 0; j < receipt.length; j++) {
-      item = receipt[j];
-      price = prices[item];
-      receipt_list = (receipt_list + item + "\n");
-      sum = sum + price;
+      let item = receipt[j];
+      let price = prices[item];
+      receipt_list += item + "\n";
+      sum += price;
     }
-    console.log()
+    setElementText("customer" + i.toString(), customer);
+    setElementText("receipt" + i.toString(), receipt_list);
+    setElementText("total" + i.toString(), sum);
   }
 }
