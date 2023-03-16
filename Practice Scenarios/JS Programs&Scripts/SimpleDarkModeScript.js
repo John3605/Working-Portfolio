f7c5581a4475a9e625c87d72f28baa733e7a4a10
John3605 <john3605.live@gmail.com>
2023-03-15 23:57:49 -0700

    Create

---
+++ Practice Scenarios/JS Programs&Scripts/SimpleDarkModeScript.js
@@ -0,0 +1,16 @@
+let mode = "light";
+let color = "ghostWhite";
+let image = "/img/light-icon.png";
+let label = "Light Mode";
+
+if (mode === "dark") {
+  color = "darkSlateBlue";
+  image = "/img/dark-icon.png";
+  label = "Dark Mode";
+}
+else if (mode === "light") {
+  color = "ghostWhite";
+  image = "/img/light-icon.png";
+  label = "Light Mode";
+}
+else {}
