bcd6e6b6ff5f0820fd1e0b0ca0da147ccac1f1ce
John3605 <john3605.live@gmail.com>
2023-03-24 00:00:06 -0700

    Update SimpleLightSwitchScript.js

--- Practice Scenarios/JS Programs&Scripts/SimpleLightSwitchScript.js
+++ Practice Scenarios/JS Programs&Scripts/SimpleLightSwitchScript.js
@@ -3,3 +3,9 @@ let lightsOn = false;
 
 console.log("Daytime?");
 console.log(isDay);
+console.log("Lights on?");
+console.log(lightsOn);
+
+isDay = false;
+lightsOn = !isDay;
+console.log("Is it Daytime? " + isDay + "How about the lights, are they on? " + lightsOn);
